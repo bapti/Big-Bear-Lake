3695ea48a1c2cff3feef6e2f5f30d4353c0a51cb
neil <neil@neil-VirtualBox.(none)>
2012-07-04 19:47:46 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BigBearLake::Application.config.secret_token = 'e81b4e8a3725fc2a8be218637a14bc7bf9cf3719e3bf766fd6b275663819f4ea4b113a77550ae81a46c06168e27a2100a1fd4e702868e89da7fc53194a45781a'
